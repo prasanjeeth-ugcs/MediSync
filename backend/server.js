5605837e887aff215d586f751251b4bd23234e57
Prasanjeeth <2023ugcs093@nitjsr.ac.in>
2026-07-21 17:43:51 +0530

    cors solved

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ app.use(cors({
     'https://medimind-frontend-three.vercel.app',
     'https://medimind-admin-green.vercel.app',
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'http://localhost:5176'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
